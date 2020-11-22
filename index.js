eca4ea83d090df7706899178bd2f69d7bd73ed77
rashila01 <rashila.mhr123@gmail.com>
2020-11-22 09:05:57 +0545

    mongoose schema registration process complete

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User=mongoose.model('User');
+
 
 const app = express();
 const port = 8000;
 
-let blogs = [{
-	id: 1,
-	title: 'hello'
-},
-{
-	id: 2,
-	title: 'world'
-}];
+let blogs = []; //{id:1, title:''}
 
+app.use(bodyParser.json());
 
-app.get('/blog',(req, resp) => {
+//query all stored blogs
+app.get('/blogs',(req, resp) => {
 	resp.json(blogs);
 });
 
+//store a user
 app.post('/blog',(req, res) => {
-	res.json({method: req.method});
+	// let{title}=req.body;
+
+	let title=req.body.title;
+	let data = {
+		id: blogs.length + 1,
+		title : title
+	}
+	blogs.push(data)
+	res.json(data);
 });
 
+//Update a blog by id
 app.put('/blog/:id',(req, res) => {
-	res.json({method: req.method});
+	let blog = blogs.filter((value) => {
+	return value.id === parseInt(req.params.id)
+	});
+	if(!data[0]){
+		return res.status(404).json({error: 'Data not found'});
+	}
+	let title = req.body.title;
+	blogs.forEach((value, index)=>{
+		if(value.id === parseInt(req.params.id)){
+			blogs[index].title = title;
+		}
+	})
+	res.json({id: req.params.id, title:title});
 });
 
+//query a blog through given data
 app.get('/blog/:id', (req, res) => {
 	let data = blogs.filter((value) => value.id === parseInt(req.params.id));
 	if(!data[0]){
@@ -33,13 +55,29 @@ app.get('/blog/:id', (req, res) => {
 	res.json(data[0]);
 })
 
+//delete a blog by given id
 app.delete('/blog/:id', (req, res) => {
-	res.json({method: req.method});
-})
+	let blog = blogs.filter((value) => {
+		return value.id === parseInt(req.params.id)
+		});
+		if(!data[0]){
+			return res.status(404).json({error: 'Data not found'});
+		}
+		let deleteIndex;
+		blogs.forEach((value, index)=>{
+			if(value.id === parseInt(req.params.id)){
+				deleteIndex=index;
+			}
+		})
+		blogs.splice(deleteIndex, 1);
 
+	res.status(204).json({});
+})
 app.listen(port, () => {
 	console.log(`Application is running on port: ${port}`);
 })
+
+module.exports = app
 
 
 
